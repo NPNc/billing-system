d2fe06946510c8c2453f443f4b3909f848e4a601
npnam <nam.nguyen1>
2023-12-26 18:38:53 +0700

    setup field text

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ root = Tk()
 root.title('Retail Billing System')
 root.geometry('1270x685')
 root.iconbitmap('resource/icon.ico')
-headingLabel = Label(root, text='Retail Billing System')
-headingLabel.pack()
+
+headingLabel = Label(root, text='Retail Billing System', font=('times new roman', 30, 'bold'), bg='gray20', fg='gold')
+headingLabel.pack(fill=X)
+
+customer_details_frame = LabelFrame(root, text='Customer Details', font=('Times new roman', 15, 'bold'))
+customer_details_frame.pack()
+
+nameLabel = Label(customer_details_frame, text='Name')
+nameLabel.grid(row=0, column=0, padx=10, pady=5)
+
+nameEntry = Entry(customer_details_frame, font=('arial', 15), width=18)
+nameEntry.grid(row=0, column=1, padx=10, pady=5)
+
+phoneLabel = Label(customer_details_frame, text='Phone')
+phoneLabel.grid(row=0, column=2, padx=10, pady=5)
+
+phoneEntry = Entry(customer_details_frame, font=('arial', 15), width=18)
+phoneEntry.grid(row=0, column=3, padx=10, pady=5)
+
 root.mainloop()
