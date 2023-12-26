a009a69c19f7578422595cb59801b82c5d725858
npnam <nam.nguyen1>
2023-12-26 17:26:08 +0700

    init project

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title('Retail Billing System')
+root.geometry('1270x685')
+root.iconbitmap('resource/icon.ico')
+headingLabel = Label(root, text='Retail Billing System')
+headingLabel.pack()
+root.mainloop()
